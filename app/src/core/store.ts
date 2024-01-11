2f7de216bce8a39f4a04a9d95ab2d69a86e3ddb1
malo <SainteCroquette>
2024-01-11 12:46:03 +0100

    :building_construction: Moved from cra (webpack) to vitejs, replaced redux by zustand; added swc devtools

--- app/src/core/store.ts
+++ app/src/core/store.ts
@@ -1,17 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counterSlice';
+import create from 'zustand';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+interface CounterState {
+    count: number;
+    increment: () => void;
+    decrement: () => void;
+}
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export const useStore = create<CounterState>((set) => ({
+    count: 0,
+    increment: () => set((state) => ({count: state.count + 1})),
+    decrement: () => set((state) => ({count: state.count - 1})),
+}));
