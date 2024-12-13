4f4f866b1075ee7f1745ad7462720aad11c5ba72
SainteCroquette <saintecroquette@github.com>
2024-12-13 18:49:26 +0100

    :arrow_up: Upgraded dependencies without breaking changes

---
+++ tsconfig.base.ts
@@ -0,0 +1,10 @@
+import { CompilerOptions } from 'typescript';
+
+const config: CompilerOptions = {
+    target: 'es5',
+    module: 'commonjs',
+    strict: true,
+    // Other options...
+};
+
+export default config;
